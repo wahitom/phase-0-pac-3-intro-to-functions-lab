a176dd9d1a398c5a4874a308236bc07ff8f4f5fe
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-16 23:14:38 +0300

    Conpleted Assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(hello){
+    return hello.toUpperCase();
+}
+
+function whisper(HELLO){
+    return HELLO.toLowerCase();
+}
+
+function logShout(hello){
+    let text = hello.toUpperCase();
+    console.log(text);
+}
+
+function logWhisper(HELLO){
+    let logText = HELLO.toLowerCase();
+    console.log(logText);
+}
+
+function sayHiToHeadphonedRoommate(string){
+
+    if (string === string.toLowerCase()){
+        return "I can't hear you!"
+    }
+    else if(string === string.toUpperCase()){
+        return "YES INDEED!"
+    }
+    else if(string === "Let's have dinner together!"){
+        return "I would love to!"
+    }   
+} 
+console.log(sayHiToHeadphonedRoommate('Let\'s have dinner together'))
+
+
