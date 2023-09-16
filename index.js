8229b8d4cba5a15c1825e540cbae6a74d350af83
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-16 23:29:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ function logWhisper(HELLO){
 function sayHiToHeadphonedRoommate(string){
 
     if (string === string.toLowerCase()){
-        return "I can't hear you!"
+        return "I can't hear you!";
     }
     else if(string === string.toUpperCase()){
-        return "YES INDEED!"
+        return "YES INDEED!";
     }
     else if(string === "Let's have dinner together!"){
-        return "I would love to!"
+        return "I would love to!";
     }   
 } 
 sayHiToHeadphonedRoommate('Let\'s have dinner together')
